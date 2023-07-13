610fdcccbb68c9e29cb0b983370a3b51db0bc04f
Yang-ah <yangah0409@gmail.com>
2023-07-13 19:49:03 +0900

    Design: 전역스타일(폰트, 배경색), assets/svgs 추가

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -3,18 +3,24 @@ import emotionReset from 'emotion-reset';
 
 const style = css`
   ${emotionReset}
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 
   * {
     margin: 0;
     padding: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
   }
 
   body {
     box-sizing: border-box;
     display: flex;
     justify-content: center;
-    background-color: #c7d1db;
     align-items: center;
+    color: #1f2328;
+    background-color: #f6f8fa;
   }
 
   button {
@@ -25,8 +31,7 @@ const style = css`
   }
 
   a {
-    color: #000;
-    text-decoration: none;
+    color: #1f2328;
     outline: none;
   }
 `;

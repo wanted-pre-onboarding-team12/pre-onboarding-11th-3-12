1055fdfabc8ed0bf3185ce1cb03b188363b6f065
dokimion24 <leecy3610@gmail.com>
2023-07-14 15:52:51 +0900

    Feat: issue 상태(detailPage에서 사용되는 데이터) 추가

--- src/contexts/IssuesContext.tsx
+++ src/contexts/IssuesContext.tsx
@@ -1,53 +1,82 @@
 import React, { createContext, useReducer, useContext, Dispatch } from 'react';
+import { IssueType } from '../types/issue';
 
 type State = {
   loading: boolean;
-  data: any;
+  data: IssueType | [];
   error: any;
+};
+
+type InitialState = {
+  issues: State;
+  issue: { data: null };
 };
 
 export type Action =
   | { type: 'GET_ISSUES' }
-  | { type: 'GET_ISSUES_SUCCESS'; data: any }
+  | { type: 'GET_ISSUES_SUCCESS'; data: IssueType }
   | { type: 'GET_ISSUES_ERROR'; error: any }
-  | { type: 'GET_ISSUE_DETAIL'; payload: string };
+  | { type: 'GET_ISSUE_DETAIL'; data: IssueType };
 
-const initialState: State = {
-  loading: false,
-  data: [],
+const loadingState = (data: any) => ({
+  loading: true,
+  data,
   error: null,
+});
+
+const success = (data: any) => ({
+  loading: false,
+  data,
+  error: null,
+});
+
+// 실패했을 때의 상태 만들어주는 함수
+const error = (error: any) => ({
+  loading: false,
+  data: null,
+  error: error,
+});
+
+const initialState: InitialState = {
+  issues: {
+    loading: false,
+    data: [],
+    error: null,
+  },
+  issue: {
+    data: null,
+  },
 };
 
-const issuesReducer = (state: State, action: Action): State | undefined => {
+const issuesReducer = (state: InitialState, action: Action): any => {
   switch (action.type) {
     case 'GET_ISSUES':
+      console.log(state);
+
       return {
         ...state,
-        loading: true,
+        issues: loadingState(state.issues.data),
       };
 
     case 'GET_ISSUES_SUCCESS':
       return {
-        loading: false,
-        data: [...state.data, ...action.data],
-        error: null,
+        ...state,
+        //@ts-ignore
+        issues: success([...state.issues.data, ...action.data]),
       };
 
     case 'GET_ISSUES_ERROR':
       return {
-        loading: false,
-        data: null,
-        error: action.error,
+        ...state,
+        issues: error(action.error),
       };
 
     case 'GET_ISSUE_DETAIL':
-      const filteredData = state.data.filter(
-        (issue: any) => issue.id === action.payload,
-      );
-      localStorage.setItem('data', JSON.stringify(filteredData[0]));
+      console.log(action);
+
       return {
         ...state,
-        data: filteredData,
+        issue: success(action.data),
       };
 
     default:

55b9b3c23c8d4246ae10c9f9fb004ca71c3ee06d
dokimion24 <leecy3610@gmail.com>
2023-07-14 15:53:55 +0900

    Feat: issue detail api 함수 호출

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import Header from '../../components/Header';
 import IssueDetail from '../../components/IssueDetail';
+import {
+  useIssuesDispatch,
+  useIssuesState,
+} from '../../contexts/IssuesContext';
+import { getIssue } from '../../service/service';
+import { useParams } from 'react-router';
 
 const Detail = () => {
-  const issue = JSON.parse(localStorage.getItem('data') || '{}');
+  const { id } = useParams();
+  console.log(id);
+  const dispatch = useIssuesDispatch();
+  //@ts-ignore
+  const { issue } = useIssuesState();
+
+  useEffect(() => {
+    getIssue(dispatch, Number(id));
+  }, [id]);
+
   return (
     <Wrap>
       <Header />
-      <IssueDetail issue={issue} />
+      <IssueDetail issue={issue.data} />
     </Wrap>
   );
 };
